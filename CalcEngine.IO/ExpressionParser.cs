bdc98f88188afec59893a6b35f73cdaaf95ab19f
kjozk <kjozk.is.cyborg+github@gmail.com>
2024-12-28 15:00:59 +0900

    コメントを追加

--- CalcEngine.IO/ExpressionParser.cs
+++ CalcEngine.IO/ExpressionParser.cs
@@ -4,9 +4,16 @@ using CalcEngine.IO.Operators;
 
 namespace CalcEngine.IO
 {
+    /// <summary>
+    /// 文字列から数式を解析するクラス。
+    /// </summary>
+    /// <typeparam name="TResult">数式の結果の型</typeparam>
     public static class ExpressionParser<TResult>
             where TResult : struct, IConvertible, IComparable, IEquatable<TResult>
     {
+        /// <summary>
+        /// 算術演算子のリスト
+        /// </summary>
         private static readonly List<BinaryOperator<TResult, TResult>> ArithmeticOperators = new List<BinaryOperator<TResult, TResult>>
         {
                 new BinaryOperator<TResult, TResult>("+", 1, (a, b) => (dynamic)a + (dynamic)b),
@@ -17,6 +24,9 @@ namespace CalcEngine.IO
                 new BinaryOperator<TResult, TResult>("÷", 2, (a, b) => (dynamic)a / (dynamic)b)
         };
 
+        /// <summary>
+        /// 比較演算子のリスト
+        /// </summary>
         private static readonly List<BinaryOperator<TResult, bool>> ComparisonOperators = new List<BinaryOperator<TResult, bool>>
         {
                 new BinaryOperator<TResult, bool>(">", 0, (a, b) => (dynamic)a > (dynamic)b),
@@ -26,20 +36,33 @@ namespace CalcEngine.IO
                 new BinaryOperator<TResult, bool>("==", 0, (a, b) => (dynamic)a == (dynamic)b)
         };
 
+        /// <summary>
+        /// 単項演算子のリスト
+        /// </summary>
         private static readonly List<UnaryOperator<TResult, TResult>> UnaryOperators = new List<UnaryOperator<TResult, TResult>>
         {
                 new UnaryOperator<TResult, TResult>("+", 4, a => +(dynamic)a),
                 new UnaryOperator<TResult, TResult>("-", 4, a => -(dynamic)a),
         };
 
+        // 括弧演算子
         private static readonly ParenthesisOperator LeftParenthesis = new ParenthesisOperator("(", 3);
         private static readonly ParenthesisOperator RightParenthesis = new ParenthesisOperator(")", 3);
 
-        public static bool TryParseArithmeticExpression(string expression, out IExpression<TResult>? result)
+        /// <summary>
+        /// 式を解析するメソッド
+        /// </summary>
+        /// <param name="expression">式文字列</param>
+        /// <param name="result">解析結果の式木</param>
+        /// <returns>
+        /// true: 解析成功
+        /// false: 解析失敗
+        /// </returns>
+        public static bool TryParse(string expression, out IExpression<TResult>? result)
         {
             try
             {
-                result = ParseArithmeticExpression(expression);
+                result = Parse(expression);
                 return true;
             }
             catch (SyntaxErrorException)
@@ -49,26 +72,37 @@ namespace CalcEngine.IO
             }
         }
 
-        public static IExpression<TResult> ParseArithmeticExpression(string expression)
+        /// <summary>
+        /// 式を解析するメソッド
+        /// </summary>
+        /// <param name="expression">式文字列</param>
+        /// <returns>解析結果の式木</returns>
+        /// <exception cref="SyntaxErrorException"></exception>
+        public static IExpression<TResult> Parse(string expression)
         {
-            var tokens = Tokenize(expression);
             var operandStack = new Stack<IExpression<TResult>>();
             var operatorStack = new Stack<IOperator>();
             var parenthesisStack = new Stack<ParenthesisOperator>(); // 括弧を追跡するスタック
+
+            // 式をトークンに分割
+            var tokens = Tokenize(expression);
 
             foreach (var token in tokens)
             {
                 if (TryParseToken(token, out TResult number))
                 {
+                    // 数値の場合はオペランドスタックに追加
                     operandStack.Push(new ConstantExpression<TResult>(number));
                 }
                 else if (token == LeftParenthesis.Symbol)
                 {
-                    operatorStack.Push(LeftParenthesis);
+                    // 左括弧の場合はオペレータスタックに追加
+                    operatorStack.Push(LeftParenthesis);    // TODO: ここでLeftParenthesisを使うのはおかしい
                     parenthesisStack.Push(LeftParenthesis); // 左括弧をスタックに追加
                 }
                 else if (token == RightParenthesis.Symbol)
                 {
+                    // 右括弧の場合は対応する左括弧までオペレータを適用
                     while (operatorStack.Count > 0 && operatorStack.Peek() != LeftParenthesis)
                     {
                         ApplyOperator(operandStack, operatorStack.Pop());
@@ -81,10 +115,12 @@ namespace CalcEngine.IO
                 }
                 else if (operandStack.Count == 0 && UnaryOperators.Any(op => op.Symbol == token))
                 {
+                    // 式の先頭に単項演算子がある場合、オペランドスタックに追加
                     operatorStack.Push(UnaryOperators.First(op => op.Symbol == token));
                 }
                 else if (ArithmeticOperators.Any(op => op.Symbol == token))
                 {
+                    // 四則演算子の場合はオペレータスタックに追加
                     while (operatorStack.Count > 0 && HasHigherPrecedence(operatorStack.Peek(), token))
                     {
                         ApplyOperator(operandStack, operatorStack.Pop());
@@ -105,27 +141,33 @@ namespace CalcEngine.IO
             // 解析後に括弧が残っていれば対応が取れていない
             if (parenthesisStack.Count > 0)
             {
-                throw new SyntaxErrorException("対応する右括弧がありません。");
+                throw new SyntaxErrorException("対応する右括弧がありません。左括弧が {parenthesisStack.Count} 個多いです。 expression=\"{expression}\", ");
             }
 
             if (operandStack.Count != 1)
             {
-                throw new SyntaxErrorException($"数式の構文が無効です。数式のルートが複数あります。{string.Join(",", operandStack)}");
+                throw new SyntaxErrorException($"数式の構文が無効です。数式のルートが複数あります。expression=\"{expression}\", operandStack=[{string.Join(",", "\"" + operandStack + "\"")}]");
             }
 
             return operandStack.Pop();
         }
 
+        /// <summary>
+        /// 式をトークンに分割するメソッド
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
         private static List<string> Tokenize(string expression)
         {
             var tokens = new List<string>();
             var number = new StringBuilder();
-            bool lastCharWasOperator = true;
+            bool lastCharWasOperator = true;    // 式の先頭に単項演算子がある場合を考慮して初期値をtrueに設定
 
             foreach (var ch in expression)
             {
-                if (char.IsDigit(ch) || ch == '.')
+                if (char.IsDigit(ch) || ch == '.')  // TODO: ドイツ語の場合はカンマも許可すべき
                 {
+                    // 数値の場合は数値文字列に追加
                     number.Append(ch);
                     lastCharWasOperator = false;
                 }
@@ -157,6 +199,7 @@ namespace CalcEngine.IO
             return tokens;
         }
 
+        // トークンを数値に変換するメソッド
         private static bool TryParseToken(string token, out TResult result)
         {
             try
@@ -171,6 +214,7 @@ namespace CalcEngine.IO
             }
         }
 
+        // 演算子を適用するメソッド
         private static void ApplyOperator(Stack<IExpression<TResult>> operandStack, IOperator operatorSymbol)
         {
             if (operatorSymbol is UnaryOperator<TResult, TResult> unaryOperator)
@@ -213,6 +257,7 @@ namespace CalcEngine.IO
             throw new SyntaxErrorException($"無効な演算子: {operatorSymbol}");
         }
 
+        // 演算子の優先順位を比較するメソッド
         private static bool HasHigherPrecedence(IOperator operator1, string operator2)
         {
             // 括弧は優先順位の比較を行わない
@@ -229,6 +274,7 @@ namespace CalcEngine.IO
             return precedence1 >= precedence2;
         }
 
+        // 演算子の優先順位を取得するメソッド
         private static int GetPrecedence(string operatorSymbol)
         {
             var arithmeticOperator = ArithmeticOperators.FirstOrDefault(op => op.Symbol == operatorSymbol);
@@ -240,11 +286,6 @@ namespace CalcEngine.IO
             }
 
             throw new SyntaxErrorException($"無効な演算子: {operatorSymbol}");
-        }
-
-        private static bool IsOperator(IOperator op)
-        {
-            return op is BinaryOperator<TResult, TResult> || op is UnaryOperator<TResult, TResult>;
         }
     }
 }
--- CalcEngine.Test/ArithmeticExpressionTests.cs
+++ CalcEngine.Test/ArithmeticExpressionTests.cs
@@ -12,27 +12,27 @@ namespace CalcEngine.Test
             IExpression<int> expression;
 
             // 3 + 4 = 7
-            expression = ExpressionParser<int>.ParseArithmeticExpression("3 + 4");
+            expression = ExpressionParser<int>.Parse("3 + 4");
             Assert.AreEqual(7, expression.Evaluate());
 
             // 1 + 1 = 2
-            expression = ExpressionParser<int>.ParseArithmeticExpression("1 + 1");
+            expression = ExpressionParser<int>.Parse("1 + 1");
             Assert.AreEqual(2, expression.Evaluate());
 
             // 0 + 0 = 0
-            expression = ExpressionParser<int>.ParseArithmeticExpression("0 + 0");
+            expression = ExpressionParser<int>.Parse("0 + 0");
             Assert.AreEqual(0, expression.Evaluate());
 
             // -1 + 1 = 0
-            expression = ExpressionParser<int>.ParseArithmeticExpression("-1 + 1");
+            expression = ExpressionParser<int>.Parse("-1 + 1");
             Assert.AreEqual(0, expression.Evaluate());
 
             // 1000000 + 2000000 = 3000000
-            expression = ExpressionParser<int>.ParseArithmeticExpression("1000000 + 2000000");
+            expression = ExpressionParser<int>.Parse("1000000 + 2000000");
             Assert.AreEqual(3000000, expression.Evaluate());
 
             // 123456789 + 987654321 = 1111111110
-            expression = ExpressionParser<int>.ParseArithmeticExpression("123456789 + 987654321");
+            expression = ExpressionParser<int>.Parse("123456789 + 987654321");
             Assert.AreEqual(1111111110, expression.Evaluate());
         }
 
@@ -43,27 +43,27 @@ namespace CalcEngine.Test
             IExpression<int> expression;
 
             // 5 - 3 = 2
-            expression = ExpressionParser<int>.ParseArithmeticExpression("5 - 3");
+            expression = ExpressionParser<int>.Parse("5 - 3");
             Assert.AreEqual(2, expression.Evaluate());
 
             // 10 - 7 = 3
-            expression = ExpressionParser<int>.ParseArithmeticExpression("10 - 7");
+            expression = ExpressionParser<int>.Parse("10 - 7");
             Assert.AreEqual(3, expression.Evaluate());
 
             // 0 - 0 = 0
-            expression = ExpressionParser<int>.ParseArithmeticExpression("0 - 0");
+            expression = ExpressionParser<int>.Parse("0 - 0");
             Assert.AreEqual(0, expression.Evaluate());
 
             // -1 - 1 = -2
-            expression = ExpressionParser<int>.ParseArithmeticExpression("-1 - 1");
+            expression = ExpressionParser<int>.Parse("-1 - 1");
             Assert.AreEqual(-2, expression.Evaluate());
 
             // 1000000 - 500000 = 500000
-            expression = ExpressionParser<int>.ParseArithmeticExpression("1000000 - 500000");
+            expression = ExpressionParser<int>.Parse("1000000 - 500000");
             Assert.AreEqual(500000, expression.Evaluate());
 
             // 987654321 - 123456789 = 864197532
-            expression = ExpressionParser<int>.ParseArithmeticExpression("987654321 - 123456789");
+            expression = ExpressionParser<int>.Parse("987654321 - 123456789");
             Assert.AreEqual(864197532, expression.Evaluate());
         }
 
@@ -74,27 +74,27 @@ namespace CalcEngine.Test
             IExpression<int> expression;
 
             // 3 * 4 = 12
-            expression = ExpressionParser<int>.ParseArithmeticExpression("3 * 4");
+            expression = ExpressionParser<int>.Parse("3 * 4");
             Assert.AreEqual(12, expression.Evaluate());
 
             // 2 × 5 = 10
-            expression = ExpressionParser<int>.ParseArithmeticExpression("2 × 5");
+            expression = ExpressionParser<int>.Parse("2 × 5");
             Assert.AreEqual(10, expression.Evaluate());
 
             // 0 * 5 = 0
-            expression = ExpressionParser<int>.ParseArithmeticExpression("0 * 5");
+            expression = ExpressionParser<int>.Parse("0 * 5");
             Assert.AreEqual(0, expression.Evaluate());
 
             // -2 × 3 = -6
-            expression = ExpressionParser<int>.ParseArithmeticExpression("-2 × 3");
+            expression = ExpressionParser<int>.Parse("-2 × 3");
             Assert.AreEqual(-6, expression.Evaluate());
 
             // 1000 * 2000 = 2000000
-            expression = ExpressionParser<int>.ParseArithmeticExpression("1000 * 2000");
+            expression = ExpressionParser<int>.Parse("1000 * 2000");
             Assert.AreEqual(2000000, expression.Evaluate());
 
             // 12345 * 6789 = 83810205
-            expression = ExpressionParser<int>.ParseArithmeticExpression("12345 * 6789");
+            expression = ExpressionParser<int>.Parse("12345 * 6789");
             Assert.AreEqual(83810205, expression.Evaluate());
         }
 
@@ -105,27 +105,27 @@ namespace CalcEngine.Test
             IExpression<int> expression;
 
             // 8 / 2 = 4
-            expression = ExpressionParser<int>.ParseArithmeticExpression("8 / 2");
+            expression = ExpressionParser<int>.Parse("8 / 2");
             Assert.AreEqual(4, expression.Evaluate());
 
             // 9 ÷ 3 = 3
-            expression = ExpressionParser<int>.ParseArithmeticExpression("9 ÷ 3");
+            expression = ExpressionParser<int>.Parse("9 ÷ 3");
             Assert.AreEqual(3, expression.Evaluate());
 
             // 0 / 1 = 0
-            expression = ExpressionParser<int>.ParseArithmeticExpression("0 / 1");
+            expression = ExpressionParser<int>.Parse("0 / 1");
             Assert.AreEqual(0, expression.Evaluate());
 
             // -6 ÷ 2 = -3
-            expression = ExpressionParser<int>.ParseArithmeticExpression("-6 ÷ 2");
+            expression = ExpressionParser<int>.Parse("-6 ÷ 2");
             Assert.AreEqual(-3, expression.Evaluate());
 
             // 1000000 / 2 = 500000
-            expression = ExpressionParser<int>.ParseArithmeticExpression("1000000 / 2");
+            expression = ExpressionParser<int>.Parse("1000000 / 2");
             Assert.AreEqual(500000, expression.Evaluate());
 
             // 123456789 / 3 = 41152263
-            expression = ExpressionParser<int>.ParseArithmeticExpression("123456789 / 3");
+            expression = ExpressionParser<int>.Parse("123456789 / 3");
             Assert.AreEqual(41152263, expression.Evaluate());
         }
 
@@ -136,35 +136,35 @@ namespace CalcEngine.Test
             IExpression<double> expression;
 
             // 3 + 5 * 2 = 13
-            expression = ExpressionParser<double>.ParseArithmeticExpression("3 + 5 * 2");
+            expression = ExpressionParser<double>.Parse("3 + 5 * 2");
             Assert.AreEqual(13, expression.Evaluate());
 
             // 10 - 2 / 2 = 9
-            expression = ExpressionParser<double>.ParseArithmeticExpression("10 - 2 / 2");
+            expression = ExpressionParser<double>.Parse("10 - 2 / 2");
             Assert.AreEqual(9, expression.Evaluate());
 
             // 1 + 2 * 3 - 4 = 3
-            expression = ExpressionParser<double>.ParseArithmeticExpression("2 * 3 + 4 / 2");
+            expression = ExpressionParser<double>.Parse("2 * 3 + 4 / 2");
             Assert.AreEqual(8, expression.Evaluate());
 
             // 10 / 2 - 3 * 2 = -1
-            expression = ExpressionParser<double>.ParseArithmeticExpression("10 / 2 - 3 * 2");
+            expression = ExpressionParser<double>.Parse("10 / 2 - 3 * 2");
             Assert.AreEqual(-1, expression.Evaluate());
 
             // 1 + 2 * 3 = 7
-            expression = ExpressionParser<double>.ParseArithmeticExpression("1 + 2 * 3");
+            expression = ExpressionParser<double>.Parse("1 + 2 * 3");
             Assert.AreEqual(7, expression.Evaluate());
 
             // (1 + 2) * 3 = 9
-            expression = ExpressionParser<double>.ParseArithmeticExpression("(1 + 2) * 3");
+            expression = ExpressionParser<double>.Parse("(1 + 2) * 3");
             Assert.AreEqual(9, expression.Evaluate());
 
             // 1000000 + 2000000 * 3 = 7000000
-            expression = ExpressionParser<double>.ParseArithmeticExpression("1000000 + 2000000 * 3");
+            expression = ExpressionParser<double>.Parse("1000000 + 2000000 * 3");
             Assert.AreEqual(7000000, expression.Evaluate());
 
             // (123456789 + 987654321) / 2 = 555555555
-            expression = ExpressionParser<double>.ParseArithmeticExpression("(123456789 + 987654321) / 2");
+            expression = ExpressionParser<double>.Parse("(123456789 + 987654321) / 2");
             Assert.AreEqual(555555555, expression.Evaluate());
         }
 
@@ -175,27 +175,27 @@ namespace CalcEngine.Test
             IExpression<double> expression;
 
             // -10 = -10
-            expression = ExpressionParser<double>.ParseArithmeticExpression("-10");
+            expression = ExpressionParser<double>.Parse("-10");
             Assert.AreEqual(-10, expression.Evaluate());
 
             // 5 + -3 = 2
-            expression = ExpressionParser<double>.ParseArithmeticExpression("5 + -3");
+            expression = ExpressionParser<double>.Parse("5 + -3");
             Assert.AreEqual(2, expression.Evaluate());
 
             // -5 - -3 = -2
-            expression = ExpressionParser<double>.ParseArithmeticExpression("-5 - -3");
+            expression = ExpressionParser<double>.Parse("-5 - -3");
             Assert.AreEqual(-2, expression.Evaluate());
 
             // -2 * -3 = 6
-            expression = ExpressionParser<double>.ParseArithmeticExpression("-2 * -3");
+            expression = ExpressionParser<double>.Parse("-2 * -3");
             Assert.AreEqual(6, expression.Evaluate());
 
             // -1000000 + 2000000 = 1000000
-            expression = ExpressionParser<double>.ParseArithmeticExpression("-1000000 + 2000000");
+            expression = ExpressionParser<double>.Parse("-1000000 + 2000000");
             Assert.AreEqual(1000000, expression.Evaluate());
 
             // -123456789 - 987654321 = -1111111110
-            expression = ExpressionParser<double>.ParseArithmeticExpression("-123456789 - 987654321");
+            expression = ExpressionParser<double>.Parse("-123456789 - 987654321");
             Assert.AreEqual(-1111111110, expression.Evaluate());
         }
 
@@ -204,19 +204,19 @@ namespace CalcEngine.Test
         public void InvalidExpressionTest()
         {
             // 無効なトークン
-            Assert.IsFalse(ExpressionParser<double>.TryParseArithmeticExpression("3 + @", out var _));
+            Assert.IsFalse(ExpressionParser<double>.TryParse("3 + @", out var _));
 
             // 括弧の不一致
-            Assert.IsFalse(ExpressionParser<double>.TryParseArithmeticExpression("(3 + 4", out var _));
+            Assert.IsFalse(ExpressionParser<double>.TryParse("(3 + 4", out var _));
 
             // 演算子の連続
-            Assert.IsFalse(ExpressionParser<double>.TryParseArithmeticExpression("3 +/ 4", out var _));
+            Assert.IsFalse(ExpressionParser<double>.TryParse("3 +/ 4", out var _));
 
             // オペランド不足
-            Assert.IsFalse(ExpressionParser<double>.TryParseArithmeticExpression("3 + ", out var _));
+            Assert.IsFalse(ExpressionParser<double>.TryParse("3 + ", out var _));
 
             // 無効な演算子
-            Assert.IsFalse(ExpressionParser<double>.TryParseArithmeticExpression("3 $ 4", out var _));
+            Assert.IsFalse(ExpressionParser<double>.TryParse("3 $ 4", out var _));
         }
 
         // 複雑な数式のテストケース
@@ -226,27 +226,27 @@ namespace CalcEngine.Test
             IExpression<int> expression;
 
             // (3 + 4) * 2 = 14
-            expression = ExpressionParser<int>.ParseArithmeticExpression("(3 + 4) * 2");
+            expression = ExpressionParser<int>.Parse("(3 + 4) * 2");
             Assert.AreEqual(14, expression.Evaluate());
 
             // 3 + 4 * 2 = 11
-            expression = ExpressionParser<int>.ParseArithmeticExpression("3 + 4 * 2");
+            expression = ExpressionParser<int>.Parse("3 + 4 * 2");
             Assert.AreEqual(11, expression.Evaluate());
 
             // (1 + 2) * (3 - 4) / 2 = -1
-            expression = ExpressionParser<int>.ParseArithmeticExpression("(1 + 2) * (3 - 4) / 2");
+            expression = ExpressionParser<int>.Parse("(1 + 2) * (3 - 4) / 2");
             Assert.AreEqual(-1, expression.Evaluate());
 
             // 10 / (2 + 3) = 2
-            expression = ExpressionParser<int>.ParseArithmeticExpression("10 / (2 + 3)");
+            expression = ExpressionParser<int>.Parse("10 / (2 + 3)");
             Assert.AreEqual(2, expression.Evaluate());
 
             // (2 + 3) * (4 - 1) = 15
-            expression = ExpressionParser<int>.ParseArithmeticExpression("(2 + 3) * (4 - 1)");
+            expression = ExpressionParser<int>.Parse("(2 + 3) * (4 - 1)");
             Assert.AreEqual(15, expression.Evaluate());
 
             // 5 * (6 + 2) - 3 = 37
-            expression = ExpressionParser<int>.ParseArithmeticExpression("5 * (6 + 2) - 3");
+            expression = ExpressionParser<int>.Parse("5 * (6 + 2) - 3");
             Assert.AreEqual(37, expression.Evaluate());
         }
     }
